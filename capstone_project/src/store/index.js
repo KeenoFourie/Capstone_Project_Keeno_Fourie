b1f9b61df88d14a40fd266bdc12857883f439257
KeenoFourie <keenofourie614@gmail.com>
2023-09-01 14:03:53 +0200

    courses page

--- capstone_project/src/store/index.js
+++ capstone_project/src/store/index.js
@@ -1,18 +1,40 @@
 import { createStore } from 'vuex'
 import axios from "axios";
 
-const dataUrl = "stuff";
+const dataUrl = "https://capstone-project-keeno-fourie-api.onrender.com/";
 
 
 export default createStore({
   state: {
+    // courses
+    courses: null,
+    // cookies
+    token: null,
+    // msg
+    msg: null,
   },
   getters: {
   },
   mutations: {
+    // courses
+    setCourses(state, courses) {
+      state.courses = courses
+    },
+    // view more
+    setCourse(state, course) {
+      state.course = course;
+    },
     // add appointment
     addAppointment(state, newAppointment) {
       state.appointments.push(newAppointment);
+    },
+    // cookies
+    setToken(state, token) {
+      state.token = token;
+    },
+    // setMsg
+    setMsg(state, msg) {
+      state.msg = msg
     },
   },
   actions: {
@@ -21,6 +43,24 @@ export default createStore({
     try {
       await axios.post(`${dataUrl}appointment`, appointmentdata);
       context.commit("addAppointment", appointmentdata);
+    } catch (e) {
+      context.commit("setMsg", "An error has occurred");
+    }
+  },
+  // courses
+  async fetchCourses(context) {
+    try {
+      const { data } = await axios.get(`${dataUrl}courses`);
+      context.commit("setCourses", data.results);
+    } catch (e) {
+      context.commit("setMsg", "An error has occurred")
+    }
+  },
+  // view more
+  async fetchCourse(context, courseID) {
+    try {
+      const { data } = await axios.get(`${dataUrl}course/${courseID}`);
+      context.commit("setCourse", data.result);
     } catch (e) {
       context.commit("setMsg", "An error has occurred");
     }
--- capstone_project/src/views/CoursesView.vue
+++ capstone_project/src/views/CoursesView.vue
@@ -1,15 +1,82 @@
 <template>
     <div>
+        <!-- displaying courses -->
+        <div id="courses_page">
+            <h1 id="heading_displayed_courses">Try Out One Of Our Courses...</h1>
+       </div>
+           
+         <div id="courses_displaying">
+             <div id="card" class="card text-bg-dark" v-for="courses in courses" :key="courses.courseID">
+                 <img id="courses_cards_image" :src="courses.courseImage" class="card-img" :alt="courses.courseName">
+                 <div class="card-img-overlay" id="course_text_display">
+                    <div id="line"></div>
+                     <h2 class="card-title"><b>{{ courses.courseName }}</b></h2>
+                     <h5 id="course_purpose">{{ courses.coursePurpose }}</h5>
+                     <router-link :to="{name:'singlecourse', params:{courseID: courses.courseID}}" id="view_more_button">Read More</router-link>
+                 </div>
+             </div>             
+        </div>
+        
+
 
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+    computed: {
+      courses() {
+          return this.$store.state.courses
+      }
+  },
+      mounted() {
+          this.$store.dispatch('fetchCourses')
+      }
+};
 </script>
 
 <style scoped>
-
+.card-title {
+    color: yellow;
+}
+#course_price {
+    color: red;
+    text-decoration: line-through;
+}
+#line {
+    border-top: 1px solid yellow;
+    padding: 0.5rem;
+}
+#courses_displaying {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+#courses_cards_image {
+    border: none;
+    border-radius: 0rem;
+    filter: brightness(40%);
+}
+#course_text_display {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    text-align: left;
+    padding: 2rem;
+}
+#view_more_button {
+    text-decoration: none;
+    color: rgba(255, 255, 0, 0.607);
+    font-weight: bold;
+}
+#view_more_button:hover {
+    color: yellow;
+}
+#card {
+    height: 20rem;
+    width: 20rem;
+    border-radius: 0rem;
+    margin: 2rem;
+    border: none;
+}
 </style>
