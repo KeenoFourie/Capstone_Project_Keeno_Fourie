bce7bdae8fc7edf32cd5c11563b04843cb8bdc9c
KeenoFourie <keenofourie614@gmail.com>
2023-09-01 14:34:59 +0200

    courses display one page

--- capstone_project/src/views/CourseDisplayView.vue
+++ capstone_project/src/views/CourseDisplayView.vue
@@ -1,15 +1,18 @@
 <template>
-    <div v-if="course">
-        <div class="card text-bg-dark" v-for="course in course" :key="course.courseID">
-            <img id="course_cards_image" :src="course.courseImage" class="card-img" :alt="course.courseName">
-            <div class="card-img-overlay" id="course_text_display">
-                <h2 class="card-title"><b>{{ course.courseName }}</b></h2>
-                <h5 id="course_purpose">{{ course.coursePurpose }}</h5>
-                <h5 id="course_purpose">{{ course.courseDesc }}</h5>
-                <h4 id="course_price">R{{ course.coursePrice }}</h4>
-                <h4 id="course_discount">R{{ course.discountPrice }}</h4>
+    <div>
+        <div v-for="course in course" :key="course.courseID" id="view_one_position">
+            <img id="view_once_image" :src="course.courseImage" :alt="course.courseName">
+            <div id="text_view_once">
+                <h1>{{course.courseName}}</h1>
+                <h4>{{course.coursePurpose}}</h4>   
+                <div id="line"></div>
+                <h5>{{course.courseDesc}}</h5>
+                <button id="price">
+                    <h6>R{{course.coursePrice}}</h6>
+                    <h4>R{{course.discountPrice}}</h4>
+                </button>
             </div>
-        </div> 
+        </div>
     </div>
 </template>
 
@@ -27,5 +30,48 @@ export default {
     }
 </script>
 <style  scoped>
-
+#price {
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    align-items: center;
+    margin: 2rem;
+    padding: 0.3rem;
+    border: none;
+    background-color: yellow;
+    width: 30%;
+}
+#price:hover {
+    background-color: grey;
+}
+h5 {
+    color: grey;
+}
+h1 {
+    color: yellow;
+}
+h6 {
+    color: red;
+    text-decoration: line-through;
+}
+#line {
+    border-top: 1px solid black;
+    padding: 1rem;
+}
+#text_view_once {
+    color: black;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    padding: 2rem;
+}
+#view_once_image {
+    height: 35rem;
+    margin: 2rem;
+}
+#view_one_position {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style> 
