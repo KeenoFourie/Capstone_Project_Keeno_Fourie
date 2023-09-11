74826a7cd4e18830a6374377ef9c1cb74bf0fc4f
KeenoFourie <keenofourie614@gmail.com>
2023-09-11 11:14:03 +0200

    edit

--- api/middleware/ErrorHandling.js
+++ api/middleware/ErrorHandling.js
@@ -1,6 +1,6 @@
 // error handling
 
-function errorHandling(err, res, req, next) {
+function errorHandling(err, req, res, next) {
     if (err) {
         let status = err.status || 500;
         res.json({
